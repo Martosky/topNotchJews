dd8e9e2febfc04b662257b3a7a9b7c84d25de77c
Martosky <imoniaromartins@yahoo.com>
2025-04-11 08:35:02 +0100

    writing the logic to put the card in the

---
+++ script.js
@@ -0,0 +1,47 @@
+const cards = document.getElementById("card-product")
+
+
+const cardInfo = [
+    {
+        id: 0,
+        img: "watches/watch1.webP",
+        title: "Rolex X",
+        availability: true
+    },
+    {
+        id: 1,
+        img: "watches/watch2.webP",
+        title: "Rolex z",
+        availability: false
+    },
+    {
+        id: 2,
+        img: "watches/watch3.webP",
+        title: "Rolex a",
+        availability: true
+    },
+    {
+        id: 3,
+        img: "watches/watch4.webP",
+        title: "Rolex c",
+        availability: true
+    },
+    {
+        id: 4,
+        img: "watches/watch5.webP",
+        title: "Rolex b",
+        availability: true
+    },
+    {
+        id: 5,
+        img: "watches/watch6.webP",
+        title: "Rolex d",
+        availability: true
+    },
+    {
+        id: 6,
+        img: "watches/watch7.webP",
+        title: "Rolex e",
+        availability: false
+    }
+]
